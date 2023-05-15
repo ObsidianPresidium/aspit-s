909879ae1cf7881d0d598d54d9c40e6d97d33ac2
ObsidianPresidium <emil@aarestrup.nu>
2023-05-15 11:16:43 +0200

    add: factorial exercise script

---
+++ S0120_factorial_exercise.py
@@ -0,0 +1,50 @@
+"""
+Opgave "factorial":
+
+Som altid skal du læse hele øpgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Alt, hvad du har brug for at vide om funktioner for at løse denne øvelse, finder du i jupiter-notesbogen S0040_basics.ipynb.
+
+Skriv en funktion med navnet "factorial".
+Rul ned for at finde det sted i denne fil, hvor du skal skrive funktionen ind.
+
+Funktionen factorial skal ...
+    have én parameter ved navn "number".
+    beregne fakultetstallet for denne parameter og gemme resultatet i en variabel med navnet " result ".
+    returnér resultatet.
+    (Hvis du ikke kan huske definitionen af fakultet: https://da.wikipedia.org/wiki/Fakultet_(matematik) eller https://en.wikipedia.org/wiki/Factorial)
+
+I hovedprogrammet...
+    Skriv en kodelinje, der gemmer det integer tal 5 i en variabel kaldet factorial_input.
+    I den næste kodelinje kalder du funktionen factorial med factorial_input som argument
+    og gem resultatet af dette funktionskald i en variabel med navnet factorial_result.
+    I den næste kodelinje udskrives "5! = 120" ved hjælp af variablerne factorial_input og factorial_result.
+
+Kør programmet. "5! = 120" skal udskrives i konsollen.
+Ændr den første linje i hovedprogrammet til "factorial_input = 6".
+Kør programmet igen.
+Denne gang skal "6! = 720" udskrives i konsollen.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
+Fortsæt derefter med den næste fil.
+"""
+
+#  Write your function below this line.
+
+def factorial(number):
+    if number == 0:
+        return 1
+
+    factorial_number = 1
+    for n in range(1, number+1):
+        factorial_number = factorial_number * n
+
+    return factorial_number
+
+# Here starts the main program. From the main program you can call your functions.
+factorial_input = 6
+factorial_result = factorial(factorial_input)
+print(f"{factorial_input}! = {factorial_result}")
