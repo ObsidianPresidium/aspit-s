bad9d6455f89c3ca5cd2e8f17fb4ce41355af02e
ObsidianPresidium <emil@aarestrup.nu>
2023-05-15 10:49:39 +0200

    add: german word salad

---
+++ S0100_test.py
@@ -0,0 +1,32 @@
+"""
+Opgave "Test pycharm og github":
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe.
+    I opgaver-projektet skal du klikke på filnavnet i vinduet Project og trykke CTRL+C.
+    Skift til løsningsprojektet, klik på hovedmappen i vinduet Project og tryk CTRL+V.
+
+Rediger kun din kopi.
+
+Kør programmet med shift+f10 (eller klik på det grønne "play"-ikon)
+
+I programkoden ændre teksten mellem anførselstegnene
+
+Commit filen til dit eget git-repository (commit betyder: sikkerhedskopier den aktuelle tilstand af dine filer som en ny version)
+    Git, Commit (eller klik på det grønne flueben)
+    skriv en meningsfuld kommentar
+    klik på "commit and push" (push betyder: upload dette commit til github)
+    push
+
+Kontroller i din webbrowser på github.com, at den ændrede fil er til stede i dit repository på github.
+
+På GitHub-websiden for dit repository skal du klikke på "Settings" (indstillinger) og sikre dig, at dit repository er offentligt (General, Change repository visibility).
+
+Hvis du ikke allerede har gjort det, skal du sende din lærer url'en til dit github-løsningsrepositorium via Teams-chat.
+URL'en ser ud som https://github.com/<dit_brugernavn/Solutions.git
+
+Derefter går du videre med den næste fil.
+"""
+
+print("Guten heute leute! Hier ist Ib Skanderborg, wieder mit die Sendung, jenseits von die dänische Grenze")
