b7332c62de246f1e4eae597e365693797a71beb1
ObsidianPresidium <emil@aarestrup.nu>
2023-06-15 10:33:49 +0200

    edit: fix positioning

--- S2010_gui_exercise1.py
+++ S2010_gui_exercise1.py
@@ -19,17 +19,25 @@ main_window.geometry("125x200")
 padx=8
 pady=4
 
-frame_1 = tk.LabelFrame(main_window, text="Container", width=100, height=175)
-frame_1.grid(row=0, column=1, padx=padx, pady=pady, columnspan=177, rowspan=177)
+frame_1 = tk.LabelFrame(main_window, text="Container")
+frame_1.grid(row=0, padx=padx, pady=pady)
+
+frame_1.pack(fill="both", expand=True, padx=padx, pady=pady)
 
 id_label = tk.Label(frame_1, text="Id")
-id_label.grid(row=1, column=1)
+id_label.grid(row=1)
+frame_1.rowconfigure(1, weight=1)
+frame_1.columnconfigure(0, weight=1)
 
-my_entry = tk.Entry(frame_1, width=6)
-my_entry.grid(row=2, column=1)
+my_entry = tk.Entry(frame_1, width=3)
+my_entry.grid(row=2)
+frame_1.rowconfigure(2, weight=1)
+# frame_1.columnconfigure(2, weight=1)
 
 my_button = tk.Button(frame_1, text="Create")
-my_button.grid(row=3, column=1)
+my_button.grid(row=3)
+frame_1.rowconfigure(3, weight=1)
+# frame_1.columnconfigure(3, weight=1)
 
 if __name__ == "__main__":
     main_window.mainloop()
