e91f7d8ae81acc5411f9daa2135e36fe5e24ec76
ObsidianPresidium <emil@aarestrup.nu>
2023-08-28 14:51:57 +0200

    add: multiple inheritance for common routines

--- danskcargo/danskcargo_data.py
+++ danskcargo/danskcargo_data.py
@@ -1,11 +1,17 @@
 from sqlalchemy.orm import declarative_base, Session  # install sqlalchemy with the command "pip install SQLAlchemy" in a terminal.
 from sqlalchemy import Column, String, Integer  # the library sqlalchemy helps us to work with a database
 from sqlalchemy import create_engine, select
+import os
 
 db = "sqlite:///danskcargo_database.db"
 Base = declarative_base()
 
-class Object:
+class Object(Base):
+    __abstract__ = True
+
+    def __init__(self):
+        super().__init__()
+
     attributes = []
 
     def convert_to_tuple(self):
@@ -14,62 +20,72 @@ class Object:
             result += i
         return result
 
-#     def valid(self):
-#         if
+    def valid(self, record):
+        def do_test(test_record):
+            if test_record.type == "INTEGER":
+                try:
+                    value = int(test_record)
+                except ValueError:
+                    return False
+                return value >= 0
+            elif test_record.type == "VARCHAR":
+                try:
+                    value = str(test_record)
+                except ValueError:
+                    return False
+            else:
+                return False
 
-class Container(Base):
+        if record == "ALL":
+            valid_attributes = []
+            for i in self.attributes:
+                valid_attributes.append(do_test(i))
+            return valid_attributes
+        else:
+            return do_test(record)
+
+class Container(Object):
     __tablename__ = "containers"
     id = Column(Integer, primary_key=True)
     weight = Column(Integer)
     destination = Column(String)
+    attributes = [id, weight, destination]
 
-    def convert_to_tuple(self):
-        return self.id, self.weight, self.destination
+    def __init__(self, weight, destination):
+        super().__init__()
+        self.weight = weight
+        self.destination = destination
 
-class Aircraft(Base):
+class Aircraft(Object):
     __tablename__ = "aircraft"
     id = Column(Integer, primary_key=True)
     capacity = Column(Integer)
     registration = Column(String)
-    def convert_to_tuple(self):
-        return self.id, self.capacity, self.registration
+    attributes = [id, capacity, registration]
 
-class Transport(Base):
+    def __init__(self, capacity, registration):
+        super().__init__()
+        self.capacity = capacity
+        self.registration = registration
+
+class Transport(Object):
     __tablename__ = "transport"
     id = Column(Integer, primary_key=True)
     date = Column(String)
     container_id = Column(Integer)
     aircraft_id = Column(Integer)
-    def convert_to_tuple(self):
-        return self.id, self.date, self.container_id, self.aircraft_id
+    attributes = [id, date, container_id, aircraft_id]
 
-# In case I need this for later...
-# to_data = [
-#     Container(weight=3003, destination="Helsinki"),
-#     Container(weight=, destination=""),
-#     Container(weight=, destination=""),
-#     Container(weight=, destination=""),
-#     Container(weight=, destination=""),
-#     Container(weight=, destination=""),
-#     Container(weight=, destination=""),
-#     Container(weight=, destination=""),
-#     Container(weight=, destination=""),
-#     Container(weight=, destination=""),
-#
-#     Aircraft(capacity=, registration=""),
-#     Aircraft(capacity=, registration=""),
-#     Aircraft(capacity=, registration=""),
-#     Aircraft(capacity=, registration=""),
-#     Aircraft(capacity=, registration=""),
-#     Aircraft(capacity=, registration=""),
-#     Aircraft(capacity=, registration=""),
-#     Aircraft(capacity=, registration=""),
-#     Aircraft(capacity=, registration=""),
-#     Aircraft(capacity=, registration=""),
-#
-#     Transport(date="", container_id=, aircraft_id=),
-#     Transport(date="", container_id=, aircraft_id=),
-#     Transport(date="", container_id=, aircraft_id=),
-#     Transport(date="", container_id=, aircraft_id=),
-#     Transport(date="", container_id=, aircraft_id=),
-# ]
+    def __init__(self, date, container_id, aircraft_id):
+        super().__init__()
+        self.date = date
+        self.container_id = container_id
+        self.aircraft_id = aircraft_id
+
+engine = create_engine(db, echo=False, future=True)
+Base.metadata.create_all(engine)
+
+def create_data(records):
+    with Session(engine) as session:
+        session.add_all(records)
+        session.commit()
