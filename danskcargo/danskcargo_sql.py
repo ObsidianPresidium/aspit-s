b33634fd78ab13edac2b5fb7f5310f9665044213
ObsidianPresidium <emil@aarestrup.nu>
2023-08-31 15:21:23 +0200

    add: default select routines

---
+++ danskcargo/danskcargo_sql.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import declarative_base, Session  # install sqlalchemy with the command "pip install SQLAlchemy" in a terminal.
+from sqlalchemy import Column, String, Integer  # the library sqlalchemy helps us to work with a database
+from sqlalchemy import create_engine, select
+from danskcargo_data import export_engine
+import os
+
+def select_all(classparam):  # return a list of all records in classparams table
+    with Session(export_engine()) as session:
+        records = session.scalars(select(classparam))
+        result = []
+        for record in records:
+            result.append(record)
+    return result
+
+
+def get_record(classparam, record_id):  # return the record in classparams table with a certain id   https://docs.sqlalchemy.org/en/14/tutorial/data_select.html
+    with Session(export_engine()) as session:
+        # in the background this creates the sql query "select * from persons where id=record_id" when called with classparam=Person
+        record = session.scalars(select(classparam).where(classparam.id == record_id)).first()
+    return record
