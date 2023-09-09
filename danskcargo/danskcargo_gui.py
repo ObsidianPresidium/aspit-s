811c0713420f597d0ebe3b769262c4e8f5abe3df
ObsidianPresidium <emil@aarestrup.nu>
2023-09-09 19:01:57 +0200

    edit: gui progress

--- danskcargo/danskcargo_gui.py
+++ danskcargo/danskcargo_gui.py
@@ -51,6 +51,70 @@ entry_container_weather = tk.Entry(edit_frame_container, width=6)
 entry_container_weather.grid(row=1, column=3, padx=padx, pady=pady)
 
 
+frame_aircraft = tk.LabelFrame(main_window, text="Aircraft")
+frame_aircraft.grid(row=0, column=1, padx=padx, pady=pady)
+
+tree_frame_aircraft = tk.Frame(frame_aircraft)
+tree_frame_aircraft.grid(row=0, column=0, padx=padx, pady=pady)
+
+tree_scroll_aircraft = tk.Scrollbar(tree_frame_aircraft)
+tree_scroll_aircraft.grid(row=0, column=1, padx=padx, pady=pady, sticky="ns")
+tree_aircraft = ttk.Treeview(tree_frame_aircraft, yscrollcommand=tree_scroll_aircraft.set, selectmode="browse")
+tree_aircraft.grid(row=0, column=0, padx=0, pady=pady)
+tree_scroll_aircraft.config(command=tree_aircraft.yview)
+
+controls_frame_aircraft = tk.Frame(frame_aircraft)
+controls_frame_aircraft.grid(row=1, column=0, padx=padx, pady=pady)
+
+edit_frame_aircraft = tk.Frame(controls_frame_aircraft)
+edit_frame_aircraft.grid(row=0,column=0, padx=padx, pady=pady)
+label_aircraft_id = tk.Label(edit_frame_aircraft, text="ID")
+label_aircraft_id.grid(row=0, column=0, padx=padx, pady=pady)
+label_aircraft_maxweight = tk.Label(edit_frame_aircraft, text="Max.Carg.Wgt.")
+label_aircraft_maxweight.grid(row=0, column=1, padx=padx, pady=pady)
+label_aircraft_registration = tk.Label(edit_frame_aircraft, text="Registration")
+label_aircraft_registration.grid(row=0, column=2, padx=padx, pady=pady)
+entry_aircraft_id = tk.Entry(edit_frame_aircraft, width=4)
+entry_aircraft_id.grid(row=1, column=0, padx=padx, pady=pady)
+entry_aircraft_maxweight = tk.Entry(edit_frame_aircraft, width=4)
+entry_aircraft_maxweight.grid(row=1, column=1, padx=padx, pady=pady)
+entry_aircraft_registration = tk.Entry(edit_frame_aircraft, width=8)
+entry_aircraft_registration.grid(row=1, column=2, padx=padx, pady=pady)
+
+
+frame_transport = tk.LabelFrame(main_window, text="Transport")
+frame_transport.grid(row=0, column=2, padx=padx, pady=pady)
+
+tree_frame_transport = tk.Frame(frame_transport)
+tree_frame_transport.grid(row=0, column=0, padx=padx, pady=pady)
+
+tree_scroll_transport = tk.Scrollbar(tree_frame_transport)
+tree_scroll_transport.grid(row=0, column=1, padx=padx, pady=pady, sticky="ns")
+tree_transport = ttk.Treeview(tree_frame_transport, yscrollcommand=tree_scroll_transport.set, selectmode="browse")
+tree_transport.grid(row=0, column=0, padx=0, pady=pady)
+tree_scroll_transport.config(command=tree_transport.yview)
+
+controls_frame_transport = tk.Frame(frame_transport)
+controls_frame_transport.grid(row=1, column=0, padx=padx, pady=pady)
+
+edit_frame_transport = tk.Frame(controls_frame_transport)
+edit_frame_transport.grid(row=0,column=0, padx=padx, pady=pady)
+label_transport_id = tk.Label(edit_frame_transport, text="ID")
+label_transport_id.grid(row=0, column=0, padx=padx, pady=pady)
+label_transport_date = tk.Label(edit_frame_transport, text="Date")
+label_transport_date.grid(row=0, column=1, padx=padx, pady=pady)
+label_transport_containerid = tk.Label(edit_frame_transport, text="Container ID")
+label_transport_containerid.grid(row=0, column=2, padx=padx, pady=pady)
+label_transport_aircraftid = tk.Label(edit_frame_transport, text="Aircraft ID")
+label_transport_aircraftid.grid(row=0, column=3, padx=padx, pady=pady)
+entry_transport_id = tk.Entry(edit_frame_transport, width=4)
+entry_transport_id.grid(row=1, column=0, padx=padx, pady=pady)
+entry_transport_date = tk.Entry(edit_frame_transport, width=4)
+entry_transport_date.grid(row=1, column=1, padx=padx, pady=pady)
+entry_transport_containerid = tk.Entry(edit_frame_transport, width=8)
+entry_transport_containerid.grid(row=1, column=2, padx=padx, pady=pady)
+entry_transport_aircraftid = tk.Entry(edit_frame_transport, width=4)
+entry_transport_aircraftid.grid(row=1, column=3, padx=padx, pady=pady)
 
 if __name__ == "__main__":
     main_window.mainloop()
