dc07a28178344c6655ed8d36db3888ad8f13c598
ObsidianPresidium <emil@aarestrup.nu>
2023-12-02 14:51:38 +0100

    add: functions for creation, deletion, and updating

--- danskcargo/danskcargo_sql.py
+++ danskcargo/danskcargo_sql.py
@@ -1,10 +1,19 @@
 from sqlalchemy.orm import declarative_base, Session  # install sqlalchemy with the command "pip install SQLAlchemy" in a terminal.
 from sqlalchemy.engine import Engine
 from sqlalchemy import Column, String, Integer  # the library sqlalchemy helps us to work with a database
-from sqlalchemy import create_engine, select, event
-from danskcargo_data import export_engine
+from sqlalchemy import create_engine, select, event, update, delete
+from danskcargo_data import export_engine, Container, Aircraft, Transport
 import os
 
+db = "sqlite:///danskcargo_database.db"
+engine = create_engine(db, echo=False, future=True)
+
+def match_attributes_and_entry_tuple(classparam, entry_tuple):
+    args = {}
+    for attribute in enumerate(classparam.attributes_names):
+        args.update({attribute[1]: entry_tuple[attribute[0]]})
+
+    return args
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
@@ -26,3 +35,38 @@ def get_record(classparam, record_id):  # return the record in classparams table
         # in the background this creates the sql query "select * from persons where id=record_id" when called with classparam=Person
         record = session.scalars(select(classparam).where(classparam.id == record_id)).first()
     return record
+
+def create_record(record):
+    with Session(engine) as session:
+        record.id = None
+        session.add(record)
+        session.commit()
+
+def update_record(classparam, entry_tuple):
+    # update a record in a table specified by classparam
+    args = match_attributes_and_entry_tuple(classparam, entry_tuple)
+    with Session(engine) as session:
+        session.execute(update(classparam).where(classparam.id == args["id"]).values(args))
+        session.commit()
+
+def delete_hard(classparam, entry_tuple):
+    # delete a record in a table specified by classparam
+    with Session(engine) as session:
+        session.execute(delete(classparam).where(classparam.id == entry_tuple[0]))
+        session.commit()
+
+def delete_soft(classparam, entry_tuple):
+    # soft delete a record in a table specified by classparam by making one of its values invalid
+    matched_dict = match_attributes_and_entry_tuple(classparam, entry_tuple)
+    if classparam == Transport:
+        matched_dict["container_id"] = -1
+    elif classparam == Aircraft:
+        matched_dict["max_weight"] = -1
+    elif classparam == Container:
+        matched_dict["weight"] = -1
+    else:
+        raise KeyError("Unknown record type passed to delete_soft function.")
+
+    with Session(engine) as session:
+        session.execute(update(classparam).where(classparam.id == matched_dict["id"]).values(matched_dict))
+        session.commit()
