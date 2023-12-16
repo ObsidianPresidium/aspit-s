eff0024f133a2ba1383dfba984f08058d42a3526
ObsidianPresidium <emil@aarestrup.nu>
2023-12-16 11:56:02 +0100

    fix: looping over records outside of engine

--- plusbus/plusbus_func.py
+++ plusbus/plusbus_func.py
@@ -14,10 +14,10 @@ def booked_passengers(target_trip_id):
     # return the amount of booked passengers on a certain trip
     with Session(pbsql.engine) as session:
         records = session.scalars(select(pbd.Booking).where(pbd.Booking.trip_id == target_trip_id))
-    num_booked_passengers = 0
-    for record in records:
-        num_booked_passengers += record.num_passengers
-    return num_booked_passengers
+        num_booked_passengers = 0
+        for record in records:
+            num_booked_passengers += record.num_passengers
+        return num_booked_passengers
 
 def space_for_input_passengers(input_num_passengers, target_trip_id):
     # return true if there's space for passengers on a trip, if there's not, return false
